4827e7b0f3f1e575c2dfeaafafc9b2578a5aed3a
廖铭峰 <mingfung@efun.com>
2017-12-28 14:54:06 +0800

    1.1.0版本 添加CocoaHttpServer库，实现PC的浏览器访问App的Server功能（暂提供IDFA供查询）。

---
+++ Carthage/Checkouts/CocoaHTTPServer/Framework/CocoaHTTPServer/CocoaHTTPServer.h
@@ -0,0 +1,19 @@
+//
+//  CocoaHTTPServer.h
+//  CocoaHTTPServer
+//
+//  Created by NeroMilk on 2017/12/27.
+//  Copyright © 2017年 Efun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaHTTPServer.
+FOUNDATION_EXPORT double CocoaHTTPServerVersionNumber;
+
+//! Project version string for CocoaHTTPServer.
+FOUNDATION_EXPORT const unsigned char CocoaHTTPServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaHTTPServer/PublicHeader.h>
+
+
